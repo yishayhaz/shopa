69734dfcee99808b60b9a5ff6f750e04bcb27a99
yishayhaz <yishay11013@gmail.com>
2022-07-22 14:54:26 +0300

    url

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL = "http://34.89.161.92:8080/api";
-// const URL = "/api";
+// const URL = "http://34.89.161.92:8080/api";
+const URL = "/api";
 
 const GET = (slug) => axios.get(`${URL}${slug}`);
 const POST = (slug, doc) => axios.post(`${URL}${slug}`, doc);
